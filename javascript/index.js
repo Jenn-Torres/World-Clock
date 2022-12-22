e2591a1ab27a1131884362accc7daa4cefd7c253
Jenn-Torres <jennifer.rosalia.gibert@gmail.com>
2022-12-22 18:14:11 +0000

    Update CityID

--- javascript/index.js
+++ javascript/index.js
@@ -1,43 +1,62 @@
 function updateTime() {
   let buenosAiresCity = document.querySelector("#buenos-aires");
-  let buenosAiresDate = buenosAiresCity.querySelector(".date");
-  let buenosAiresTime = buenosAiresCity.querySelector(".time");
-  let buenosAiresCurrentTime = moment().tz("America/Argentina/Buenos_Aires");
+  if (buenosAiresCity) {
+    let buenosAiresDate = buenosAiresCity.querySelector(".date");
+    let buenosAiresTime = buenosAiresCity.querySelector(".time");
+    let buenosAiresCurrentTime = moment().tz("America/Argentina/Buenos_Aires");
 
-  buenosAiresDate.innerHTML = moment().format("MMMM Do YYYY");
-  buenosAiresTime.innerHTML = buenosAiresCurrentTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    buenosAiresDate.innerHTML = moment().format("MMMM Do YYYY");
+    buenosAiresTime.innerHTML = buenosAiresCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let romeCity = document.querySelector("#rome");
-  let romeDate = romeCity.querySelector(".date");
-  let romeTime = romeCity.querySelector(".time");
-  let romeCurrentTime = moment().tz("Europe/Rome");
+  if (romeCity) {
+    let romeDate = romeCity.querySelector(".date");
+    let romeTime = romeCity.querySelector(".time");
+    let romeCurrentTime = moment().tz("Europe/Rome");
 
-  romeDate.innerHTML = moment().format("MMMM Do YYYY");
-  romeTime.innerHTML = romeCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+    romeDate.innerHTML = moment().format("MMMM Do YYYY");
+    romeTime.innerHTML = romeCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 
   let dubaiCity = document.querySelector("#dubai");
-  let dubaiDate = dubaiCity.querySelector(".date");
-  let dubaiTime = dubaiCity.querySelector(".time");
-  let dubaiCurrentTime = moment().tz("Asia/Dubai");
+  if (dubaiCity) {
+    let dubaiDate = dubaiCity.querySelector(".date");
+    let dubaiTime = dubaiCity.querySelector(".time");
+    let dubaiCurrentTime = moment().tz("Asia/Dubai");
 
-  dubaiDate.innerHTML = moment().format("MMMM Do YYYY");
-  dubaiTime.innerHTML = dubaiCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+    dubaiDate.innerHTML = moment().format("MMMM Do YYYY");
+    dubaiTime.innerHTML = dubaiCurrentTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
-updateTime();
-setInterval(updateTime, 1000);
 
 function updatecity(event) {
   let cityTimeZone = event.target.value;
+  let cityId = "";
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
+  if (cityName === "Argentina") {
+    cityName = "Buenos Aires";
+    cityId = "buenos-aires";
+  }
+  if (cityName === "Rome") {
+    cityId = "rome";
+  }
+  if (cityName === "Dubai") {
+    cityId = "dubai";
+  }
+
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-    <div class="city">
+    <div class="city" id=${cityId}>
           <div>
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
@@ -45,8 +64,13 @@ function updatecity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")} </small></div>
-        </div>`;
+        </div>
+        <a href="/">All cities</a>
+        `;
 }
+
+updateTime();
+setInterval(updateTime, 1000);
 
 let citiesSelect = document.querySelector("#city");
 citiesSelect.addEventListener("change", updatecity);
